cff1229cb912864dca077d79eb27f8847f50747e
Nauflsan <naufalsausan37@gmail.com>
2026-01-15 19:57:47 +0700

    Add day-4 Project : Backend

---
+++ day-4/apps/backend/src/main.ts
@@ -0,0 +1,35 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // 🔹 AKTIFKAN DTO VALIDATION (PENTING)
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Event Simple Storage DApp API')
+    .setDescription('The Simple Storage dApp')
+    .setVersion('1.0')
+    .addTag('simple-storage')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('swagger', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+
+bootstrap().catch((error) => {
+  console.error('Error during application bootstrap:', error);
+  process.exit(1);
+});
