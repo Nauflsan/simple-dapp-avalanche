720cd6584353c7f736cb9d6c6917f76c0d9aa0d5
Nauflsan <naufalsausan37@gmail.com>
2026-01-16 19:44:40 +0700

    Ready for railway deploy

--- day-4/apps/backend/src/main.ts
+++ day-4/apps/backend/src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   // 🔹 AKTIFKAN DTO VALIDATION (PENTING)
   app.useGlobalPipes(
